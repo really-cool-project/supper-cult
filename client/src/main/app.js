6fbbdd1fa479d98bb39190cef8c246c4cc983174
V School Student <student@Vs-Mac-Pro-7.local>
2017-10-02 10:10:11 -0600

    apply page added

---
+++ client/src/main/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import ApplyContainer from "./apply/ApplyContainer.js";
+
+class App extends Component {
+    render() {
+        return (
+            <ApplyContainer />
+        );
+    }
+}
+
+export default App;
